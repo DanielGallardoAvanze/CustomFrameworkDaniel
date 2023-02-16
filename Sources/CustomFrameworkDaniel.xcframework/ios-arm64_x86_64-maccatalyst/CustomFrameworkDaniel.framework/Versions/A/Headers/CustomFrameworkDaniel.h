f6c8cffabb8971c9b2d7fb4365e6e0bc0466bb7a
Daniel Gallardo <daniel.gallardo@avanze.net>
2023-02-16 15:35:33 +0100

    Version 0.0.2

---
+++ Sources/CustomFrameworkDaniel.xcframework/ios-arm64_x86_64-maccatalyst/CustomFrameworkDaniel.framework/Versions/A/Headers/CustomFrameworkDaniel.h
@@ -0,0 +1,18 @@
+//
+//  CustomFrameworkDaniel.h
+//  CustomFrameworkDaniel
+//
+//  Created by Daniel Gallardo on 16/2/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFrameworkDaniel.
+FOUNDATION_EXPORT double CustomFrameworkDanielVersionNumber;
+
+//! Project version string for CustomFrameworkDaniel.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkDanielVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFrameworkDaniel/PublicHeader.h>
+
+
